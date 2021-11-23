f206f530b4b3d59c2ed057c60d1791aacfce252a
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-23 17:37:38 +0000

    Create `Signin` component

--- src/pages/user/Signin.js
+++ src/pages/user/Signin.js
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Signin() {
-  return <h1>Signin</h1>;
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+    console.log("Inside SignIn form: ", inputName, inputValue);
+
+    setUser({
+      ...user,
+      [inputName]: inputValue,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const fetchOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...user }),
+    };
+
+    fetch("http://localhost:3030/signin", fetchOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Inside signin fetch: ", data);
+      });
+  };
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <h2>Sign In</h2>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" onChange={handleChange} />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
